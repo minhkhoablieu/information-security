384e6cfa30321ee9d3acb55fc2fc7bced2c01083
Khoa Phung <chanzi909@gmail.com>
2021-09-21 09:44:15 +0700

    Create caesar_cipher.py

---
+++ caesar_cipher.py
@@ -0,0 +1,33 @@
+class CaesarCipher:
+    def __init__(self, text, key):
+        self.text = text
+        self.key = key
+        self._lenght_text = len(text)
+
+    def encrypt(self):
+        result = ''
+        for i in range(self._lenght_text):
+            char = self.text[i]
+            if (char.isupper()):
+                result += chr((ord(char) + self.key - 65) % 26 + 65)
+
+            else:
+                result += chr((ord(char) + self.key - 97) % 26 + 97)
+        return result
+
+    def decrypt(self):
+        result = ''
+        for i in range(self._lenght_text):
+            char = self.text[i]
+            if (char.isupper()):
+                result += chr((ord(char) - self.key - 65) % 26 + 65)
+
+            else:
+                result += chr((ord(char) + self.key - 97) % 26 + 97)
+        return result
+
+
+message = "EXXEGOEXSRGI"
+key = 4
+encrypt_message = CaesarCipher(text=message, key=key).encrypt()
+print(encrypt_message)
